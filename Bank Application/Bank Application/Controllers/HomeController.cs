1e8a107060f5a3296729d78394698a98078125f6
pwn1kunwar <pwn1kunwar@gmail.com>
2025-03-14 20:44:06 +0545

    Initial Commit

---
+++ Bank Application/Bank Application/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Bank_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bank_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(BankAccount bankAccount)
+        {
+            return Content($"Account : {bankAccount.AccountNumber} has been updated with new Pin: {bankAccount.Pin}");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bank Application/Bank Application/Models/BankAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace Bank_Application.Models
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string Pin { get; set; }
+    }
+}
